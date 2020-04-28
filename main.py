1173a32565e43bf5047f403944cc6fa1438df54e
Douglas Wallin <wallind@mail.gvsu.edu>
2020-04-27 22:52:28 -0400

    The script is now functionally producing a decent dataset

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
-
+import re
+import csv
 import mailbox
+import numpy
+from numpy import savetxt
 
-mailboxObj = mailbox.mbox("C:/Users/Horatio/Desktop/All mail Including Spam and Trash.mbox")
 
+OUTPUT_FILE_PATH = "./bodyData.csv"
+PATH_TO_MBOX_FILE = "C:/Users/Horatio/Desktop/All mail Including Spam and Trash.mbox"
+
+mailboxObj = mailbox.mbox(PATH_TO_MBOX_FILE)
 
 observedCharsets = set()
-maxMultipartDepth = 0
 multipartEmails = 0
 emailBodyData = []
 emailsProcessed = 0
@@ -15,32 +20,46 @@ iterCount = 15
 
 for email in mailboxObj:
 	if email.is_multipart(): multipartEmails += 1
-	multipartDepth = 0	
  
 	while email.is_multipart():
-		multipartDepth += 1
 		email = email.get_payload()[0]
-		if multipartDepth > maxMultipartDepth: maxMultipartDepth = multipartDepth
 	 
 	text = email.get_payload(decode=True)
+ 
+	if (len(email.get_charsets()) == 0): print('one')
  
 	for charset in email.get_charsets():
 		if (charset != None):
 			observedCharsets.add(charset)
 			try:
 				text = text.decode(charset)
+				text = text.replace('\n', '')
+				text = text.replace('\r', '')
+				text = text.replace(',', '')
+				text = re.sub(r'<style[^>]*>([^<]+)<\/style>', '', text)
+				text = re.sub(r'<head[^>]*>([^<]+)<\/head>', '', text)
+				text = re.sub(r'<[^>]*>', '', text)
+    
+				emailBodyData.append(text)
 			except:
 				errorCount += 1
-				print(f"errored trying to use {charset} from {email.get_charsets()}")
-
-	emailBodyData.append(text)		
+				print(f"errored trying to decode payload using: {charset} from result of get_charsets: {email.get_charsets()}")
+			
 	emailsProcessed += 1
-  
+	
 
 
-print(f"Max multipart depth: {maxMultipartDepth}")
 print(f"Observed charsets: {observedCharsets}")
 print(f"Emails Processed: {emailsProcessed}")
 print(f"Harvested Data length: {len(emailBodyData)}")
 print(f"Multipart Emails: {multipartEmails}")
 print(f"Error Count: {errorCount}")
+savetxt(OUTPUT_FILE_PATH, emailBodyData, delimiter=',', fmt='%s', encoding='utf-8-sig')
+
+
+with open(OUTPUT_FILE_PATH, encoding="utf-8-sig") as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+ 
+	rows = list(csv_reader)
+	rowCount = len(rows)
+	print(f"Data samples in saved CSV file: {rowCount}")
